d8af429c7068ca723efa97622ae0e6bc1ca13635
Facundo Calde <facundo_calde@icloud.com>
2025-10-09 15:18:41 -0300

    modifico htmls con script nombres

--- Frontend/js/areas.js
+++ Frontend/js/areas.js
@@ -133,3 +133,33 @@ async function eliminarArea(id) {
     }
   }
 }
+
+
+
+// 🔹 Manejo de sesión en Pacientes
+// ==========================
+const token = localStorage.getItem("token");
+const usuario = JSON.parse(localStorage.getItem("usuario"));
+
+// Si no hay token → volver al login
+if (!token) {
+  window.location.href = "index.html";
+}
+
+// Mostrar nombre dinámico en el top bar (si existe <strong id="userName">)
+if (usuario && usuario.nombreApellido) {
+  const userNameEl = document.getElementById("userName");
+  if (userNameEl) userNameEl.textContent = usuario.nombreApellido;
+}
+
+// 🔹 Botón cerrar sesión (si existe <button id="btnLogout">)
+const btnLogout = document.getElementById("btnLogout");
+if (btnLogout) {
+  btnLogout.addEventListener("click", () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("usuario");
+    window.location.href = "index.html";
+  });
+}
+
+
